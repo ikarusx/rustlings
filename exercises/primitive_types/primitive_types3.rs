e732aaeba5aef0cfc453b90842e6f150e5f38afb
ikarusx <limbar@hotmail.com>
2021-09-13 12:46:49 -0700

    [Exercises] Done with 'primitive types'.

--- exercises/primitive_types/primitive_types3.rs
+++ exercises/primitive_types/primitive_types3.rs
@@ -2,10 +2,16 @@
 // Create an array with at least 100 elements in it where the ??? is.
 // Execute `rustlings hint primitive_types3` for hints!
 
-// I AM NOT DONE
+//// I AM NOT DONE
 
 fn main() {
-    let a = ???
+    //let a = ??? // error
+    //let a = [0; 99];
+    let a = [0; 100];
+    //let a = 0..100;
+    //let a: [i32; 100] = [0; 100];
+    //let a = ['\0'; 333];
+    //let a = "A string with a hundred characters would also work for this exercise, since it is essentially chars.";
 
     if a.len() >= 100 {
         println!("Wow, that's a big array!");
